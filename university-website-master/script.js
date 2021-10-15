51b7cc9566c0c700c2603796dba38d5e688294b9
Adnen Ben Amor <91540820+adnnba@users.noreply.github.com>
2021-10-15 14:30:28 +0300

    Delete university-website-master directory

--- university-website-master/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector(".nav-links")
-
-document.querySelector("nav .fa-bars").addEventListener("click", () => {
-  nav.style.right = "0"
-})
-
-document.querySelector("nav .fa-times").addEventListener("click", () => {
-  nav.style.right = "-55vw"
-})
